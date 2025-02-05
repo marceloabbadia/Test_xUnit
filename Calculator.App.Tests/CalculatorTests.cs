fc356c12cf14c7bd117691b9b6922c64561a64a7
Marcelo Abbadia <marcelo.abbadia@gmail.com>
2025-02-05 23:47:49 +0000

    Tests with xUnit

---
+++ Calculator.App.Tests/CalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿
+namespace CalculatorApp.Tests;
+
+public class CalculatorTests
+{
+    Calculator _calculator;
+
+    public CalculatorTests()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Fact]
+    public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+    {
+        //  Arrange
+        var calculator = new Calculator();
+        var n1 = 100;
+        var n2 = 200;
+        var expected = n1 + n2;
+
+        // Act
+        int result = calculator.Add(n1, n2);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Add_TwoLargeNumbers_ReturnsCorrectSum()
+    {
+        //  Arrange
+        var calculator = new Calculator();
+        var n1 = int.MaxValue;
+
+        // Act
+        int result = calculator.Add(n1, 1);
+
+        // Assert
+        Assert.Equal(int.MinValue, result);
+    }
+
+    [Theory]
+    [InlineData(2, 3, 6)]
+    [InlineData(2, 0, 0)]
+    [InlineData(2, 30, 60)]
+    [InlineData(2, -1, -2)]
+    [InlineData(2, -3, -6)]
+    public void Sum_TwoNumbers_ReturnsCorrectProduct(int n1, int n2, int expected)
+    {
+        var actual = _calculator.Multiply(n1, n2);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Divide_DivisionByZero_ThrowsException()
+    {
+        var calculator = new Calculator();
+        Assert.Throws<DivideByZeroException>(() => calculator.Divide(12, 0));
+    }
+}
---
+++ Calculator.App/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(params int[] nums)
+        {
+            int sum = 0;
+            foreach (var num in nums)
+                sum += num;
+
+            return sum;
+        }
+
+        public double Divide(int v1, int v2)
+        {
+            if (v2 == 0)
+                throw new DivideByZeroException();
+
+            return v1 / (double)v2;
+        }
+
+        public double Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+    }
+}
